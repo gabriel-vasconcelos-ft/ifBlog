84fae28d29493cc4e07473a20f101cfec41da0fe
Gabriel Vasconcelos <97545157+gabriel-vasconcelos-ft@users.noreply.github.com>
2023-05-29 16:42:14 -0300

    index.jsx

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,49 +1,110 @@
-import "./Cadastro.css"
+/* eslint-disable no-unused-vars */
+import "./Cadastro.css";
+import React, { useState } from "react";
 
-function cadastro() {
-    return (
+function Cadastro() {
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const [formValidated, setFormValidated] = useState(false);
+  const [senhaError, setSenhaError] = useState("");
 
-<div className="backgroundvisual">
-  <div className="container w-25 p-5">
-  <form>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
 
-    <div className="mb-4">
-      <img src="./img/logo.png" alt=""></img>
+    if (event.currentTarget.checkValidity() === false) {
+      setFormValidated(true);
+      return;
+    }
+
+    if (senha !== confirmarSenha) {
+      setSenhaError("A senha e a confirmação de senha não coincidem.");
+      return;
+    }
+
+    setSenhaError(""); // Limpar mensagem de erro se as senhas coincidirem
+
+    // Envie os dados do formulário
+    console.log("Dados do formulário enviados");
+  };
+
+  const handleMatriculaKeyPress = (event) => {
+    const pattern = /[0-9]/;
+    const inputChar = String.fromCharCode(event.which);
+
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  };
+
+  return (
+    <div className="backgroundvisual">
+      <div className="container p-5">
+        <form className={`needs-validation ${formValidated ? "was-validated" : ""}`} noValidate onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <img src="./img/logo.png" alt="" />
+          </div>
+          <div className="form-label">
+            <h5>CADASTRO</h5>
+            <small id="cadastroID" className="form-text text-muted">
+              Por favor preencha os dados abaixo para realizar seu cadastro no blog
+            </small>
+          </div>
+          <div className="form-group mb-3">
+            <input type="label" className="form-control" id="nomeID" placeholder="Nome" required />
+          </div>
+          <div className="form-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="matriculaID"
+              placeholder="Matrícula"
+              onKeyPress={handleMatriculaKeyPress}
+              required
+            />
+          </div>
+          <div className="form-group mb-3">
+            <input type="email" className="form-control" id="emailID" placeholder="E-mail Institucional" required />
+          </div>
+          <div className="form-group mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="senhaID"
+              placeholder="Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group mb-4">
+            <input
+              type="password"
+              className={`form-control ${senha !== confirmarSenha ? "is-invalid" : ""}`}
+              id="confID"
+              placeholder="Confirmar Senha"
+              value={confirmarSenha}
+              onChange={(e) => setConfirmarSenha(e.target.value)}
+              required
+            />
+            {senha !== confirmarSenha && <div className="invalid-feedback">{senhaError}</div>}
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Envie uma foto sua</label>
+            <input className="form-control" type="file" id="formFile" />
+          </div>
+          <button type="submit" className="btn btn-outline-success mb-2">
+            Cadastrar
+          </button>
+          <div className="form-label">
+            <a id="cadastroID" className="form-text text-muted" type="submit">
+              Ou fazer login
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
+  );
+}
 
-  <div className="form-label">
-    <h5>CADASTRO</h5>
-    <small id="cadastroID" className="form-text text-muted"> Por favor preencha o dados abaixo para realizar seu cadastro no blog</small>
-  </div>
-
-  <div className="form-group mb-3 ">
-    <input type="label" className="form-control" id="nomeID" aria-describedby="emailHelp" placeholder="Nome"></input>
-  </div>
-  <div className="form-group mb-3 ">
-    <input type="label" className="form-control" id="matriculaID" aria-describedby="emailHelp" placeholder="Matrícula"></input>
-  </div>
-  <div className="form-group mb-3 ">
-    <input type="email" className="form-control" id="emailID" aria-describedby="emailHelp" placeholder="E-mail Institucional"></input>
-  </div>
-  <div className="form-group mb-3 ">
-    <input type="password" className="form-control" id="senhaID" placeholder="Senha"></input>
-  </div>
-  <div className="form-group mb-4 ">
-    <input type="password" className="form-control" id="confID" placeholder="Confirmar Senha"></input>
-  </div>
-  
-  <div className="custom-file mb-4">
-    <input type="file" className="custom-file-input" id="customFile"></input>
-  </div>
-
-  <button type="submit" className="btn btn-outline-success">Cadastrar</button>
-    </form>
-    </div>
-  </div>
-
-
-    )
-  }
-  
-  export default cadastro
-  
+export default Cadastro;
